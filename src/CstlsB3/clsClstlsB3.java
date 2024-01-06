aa4e1d3f6b1a5ba60c0f5f1929558d9b33c47724
AnuCasetools <anusree@rosarycollege.org>
2024-01-06 11:18:44 +0530

    first github commit

---
+++ src/CstlsB3/clsClstlsB3.java
@@ -0,0 +1,13 @@
+package CstlsB3;
+
+public class clsClstlsB3 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world B3");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author admin
+ *
+ */
+module SybcaB3Github {
+}
